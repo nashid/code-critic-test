3e1db8711418a75aa6ecb033228a349539c3546f
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-06-01 22:10:27 -0700

    Remove test

--- test.py
+++ test.py
@@ -6,8 +6,3 @@ for (int i = 0; i < 12; i++) {
     console.log("What is this language?");
     System.out.println('Is it java? Ew');
 }
-
-
-while (true) {
-    // this is a poorly written comment
-}
