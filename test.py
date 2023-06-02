8a7bea04a28b891d3d201b0484cc2be64af08144
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-06-01 22:06:50 -0700

    Test action with incorrect Python code

--- test.py
+++ test.py
@@ -6,3 +6,8 @@ for (int i = 0; i < 12; i++) {
     console.log("What is this language?");
     System.out.println('Is it java? Ew');
 }
+
+
+while (true) {
+    // this is a poorly written comment
+}
